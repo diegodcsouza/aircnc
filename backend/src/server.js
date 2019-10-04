8165dfb760df8d946ecb78845f4bfcf9f64ba75c
diegodcsouza <diegodcsouza@gmail.com>
2019-10-04 17:58:18 -0300

    Frontend - Complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -10,7 +12,10 @@ mongoose.connect('mongodb+srv://user:user123@dbaircnc-s1kse.mongodb.net/dataairc
     useUnifiedTopology:true,
 })
 
+
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
